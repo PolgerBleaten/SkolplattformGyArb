c0fdc0a452b24406e125ccdf9e6074fcb10bc8a5
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2021-10-27 10:38:10 +0200

    La till eventlisteners, fixade schemat

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 'use strict';
-
+let idag = document.querySelector("[data-idag]");
+let vecka = document.querySelector("[data-vecka]");
+let månad = document.querySelector("[data-månad]");
 let links = document.querySelectorAll("a");
 let articles = document.querySelectorAll("article");
+
 console.log(articles);
+
+idag.addEventListener("click", Idag);
+vecka.addEventListener("click", Vecka);
+månad.addEventListener("click", Månad);
 
 links.forEach((link) => {
     link.addEventListener("click", (event) => {
@@ -11,3 +18,15 @@ links.forEach((link) => {
         document.querySelector("#" + link.getAttribute("data-linked-article")).classList.remove("hidden");    
     });
 });
+
+function Idag(){
+    
+}
+
+function Vecka(){
+
+}
+
+function Månad(){
+
+}
