91df53c2e06f9fcb975f52876f3394fb99a4a827
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2022-01-23 13:02:32 +0100

    banger

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function darkmode() {
         element.style.color = "rgb(199, 175, 175)";
     });
     darkmodeimage.src = "Images/lightmode.png";
-    document.querySelectorAll("div.genomskinligBakgrund div, #ämnen h4, .kurser tbody tr:nth-last-of-type(even), .nyheter tbody tr:nth-last-of-type(even), #sidebar").forEach(element => {
+    document.querySelectorAll("div.genomskinligBakgrund div, #ämnen h4, .kurser tbody tr:nth-last-of-type(even), .nyheter tbody tr:nth-last-of-type(even), #sidebar, #veckaschema .dag:nth-last-of-type(even)").forEach(element => {
         element.style.backgroundColor = "rgba(80, 80, 80, 0.692)";
     });
     if(idagaktiv){
@@ -146,7 +146,7 @@ function darkmode() {
 
 function lightmode() {
     darkmodeactive = false;
-    document.querySelectorAll("div.genomskinligBakgrund div, #ämnen h4, .kurser tbody tr:nth-last-of-type(even), .nyheter tbody tr:nth-last-of-type(even), #sidebar").forEach(element => {
+    document.querySelectorAll("div.genomskinligBakgrund div, #ämnen h4, .kurser tbody tr:nth-last-of-type(even), .nyheter tbody tr:nth-last-of-type(even), #sidebar, #veckaschema .dag:nth-last-of-type(even)").forEach(element => {
         element.style.backgroundColor = "rgba(223, 219, 219, 0.692)";
     });
     document.querySelectorAll("button").forEach(element =>{
